5d30992b82691efb7e55abe87f5dead3eaa9be4f
chung10102004 <22520161@gm.uit.edu.vn>
2025-05-16 09:32:35 +0700

    add video

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import requests
 import json
 input = {"features": "normal"}
 
-url = "http://127.0.0.1:8888/predict"
+url = "http://192.168.28.92:8888/predict"
 data = json.dumps(input)
 
 repposense = requests.post(url, data)
